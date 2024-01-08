feca93f6199e8a96f0a2d59422d5da90156112af
Paul Holsters <paul.holsters@hotmail.com>
2024-01-08 14:15:42 +0100

    afwerken api's test backend

--- routes/mijnLijst.js
+++ routes/mijnLijst.js
@@ -6,20 +6,23 @@ const SchemaFilm = require('../models/Film')
 router.put('/add/:id/:contentId', async(req, res, next) => {
     const film =  await SchemaFilm.filmModel.findById(req.params.contentId)
     if(film){
-        Schema.mijnLijstModel.findOne({account:req.params.id}).then(res=>{
-            console.log('film found',res)
-            if(!res){
+        Schema.mijnLijstModel.findOne({account:req.params.id}).then(lijst=>{
+            if(!lijst){
                 Schema.mijnLijstModel.create({
                     account:req.params.id,
                     films:[req.params.contentId],
                     series:[]
                 })
             } else{
-                // todo fix this shit
-                res.films.push(req.params.contentId)
-                Schema.mijnLijstModel.findOneAndUpdate({account:req.params.id},{films:res.films})
-            }
-        }).catch(err => {
+                const newArrOfFilms = lijst.films.map(el=>el.toString())
+                newArrOfFilms.push(req.params.contentId)
+                console.log(newArrOfFilms)
+                Schema.mijnLijstModel.findOneAndUpdate({account:req.params.id},{films:newArrOfFilms}).then(result=>{
+                    res.status(201).json({
+                        doc:result
+                    })
+            })
+        }}).catch(err => {
             console.log('error!!!!!!')
             res.status(500).json({
                 error: err.errors
